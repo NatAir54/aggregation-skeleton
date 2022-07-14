6da0ee8994be2ffcb08898473b399b4421e901c3
Nataliia Koval <ruba.fish1500@gmail.com>
2022-07-14 20:16:38 +0300

    Fix Deposit Fix BaseDeposit Fix SpecialDeposit Fix LongDeposit

--- src/main/java/com/epam/rd/qa/aggregation/Deposit.java
+++ src/main/java/com/epam/rd/qa/aggregation/Deposit.java
@@ -1,6 +1,25 @@
 package com.epam.rd.qa.aggregation;
 
-public class Deposit {
-    // TODO Place your code here
+import java.math.BigDecimal;
+
+public abstract class Deposit {
+
+    public BigDecimal amount;
+    public int period;
+
+    public Deposit (BigDecimal depositAmount, int depositPeriod) {
+        if(depositPeriod<=0 || depositAmount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.amount = depositAmount;
+        this.period = depositPeriod;
+    }
+
+    public BigDecimal getAmount() { return amount; }
+
+    public int getPeriod() { return period; }
+
+    abstract BigDecimal income();
+
 }
 
